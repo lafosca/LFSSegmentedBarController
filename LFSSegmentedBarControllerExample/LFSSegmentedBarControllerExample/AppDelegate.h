5aa6b984b70868b2cb0d7d71d944a637709f28b6
David Cortes <david.cortes.18@gmail.com>
2015-05-07 16:05:38 +0200

    Removed example as submodule

---
+++ LFSSegmentedBarControllerExample/LFSSegmentedBarControllerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFSSegmentedBarControllerExample
+//
+//  Created by David Cortés Fulla on 7/5/15.
+//  Copyright (c) 2015 Lafosca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
